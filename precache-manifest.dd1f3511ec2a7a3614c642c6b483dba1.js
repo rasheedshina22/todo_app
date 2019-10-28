c01a41f1aea3c5619d2aaf9867bc1bb6d6cdd69c
rasheedshina22 <rasheedshinaopeyemi@gmail.com>
2019-10-28 12:24:44 +0100

    Updates

---
+++ precache-manifest.dd1f3511ec2a7a3614c642c6b483dba1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343df28d02f31282aac6dd6a67c7a6e7",
+    "url": "/todo_app/index.html"
+  },
+  {
+    "revision": "d8c98242a7690e242c56",
+    "url": "/todo_app/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "200b6c65fac60e464ceb",
+    "url": "/todo_app/static/js/2.c9f2daef.chunk.js"
+  },
+  {
+    "revision": "d8c98242a7690e242c56",
+    "url": "/todo_app/static/js/main.cff6f99b.chunk.js"
+  },
+  {
+    "revision": "6c1ee6e246643c8a5a1d",
+    "url": "/todo_app/static/js/runtime~main.da743e9d.js"
+  }
+]);
